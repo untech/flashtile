fb5021b1712b63f4483efad068bb3e7335320c06
leffuy <leflor@gmail.com>
2011-07-07 18:38:24 -0400

    Added Enumeration

--- src/flashtile.c
+++ src/flashtile.c
@@ -3,9 +3,19 @@
 //This serves as the base file for the Flash Tile source.
 #include "flashtile.h"
 
+enum CDEPTH{
+COLOR_8 = 8,
+COLOR_16 = 16
+};
+
+enum PXLSZ{
+PIXEL_8 = 8,
+PIXEL_16 = 16
+};
+
 //first function based on unit tests fill in to pass unit test
-FTile16* initFTile16(void){
-FTile16* ptiles = 0;
+FTile8* initFTile8(){
+FTile8* ptiles = 0;
 
 //stub, put your shit in here
 
