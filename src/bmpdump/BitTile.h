9a114784dc097c3a68d37bd7a0c587188c774b77
leffuy <leflor@gmail.com>
2011-07-21 14:43:29 -0400

    Reduced interface-facade header.

--- src/bmpdump/BitTile.h
+++ src/bmpdump/BitTile.h
@@ -1,6 +1,11 @@
 //These functions allow for tiles to be shoved into bitmaps and vice versa
 //this interface is intended for use with the allegro chain
 
+//DeSignNotes: The BitTile interface serves as a facade for the functionality
+//of the entire system along multiple platforms
+
+//in theory implementations of the facade result in platform compatibility
+
 class IBitTile
 {
       public:
@@ -10,11 +15,8 @@ class IBitTile
 
 };
 
-//Streams tiles from a map to a bmp object
-void createIBitTile(){
-   
 
 
 
-}
+
 
