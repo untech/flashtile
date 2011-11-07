f839b4daac0e8cb26c24ef46d16f172a0ede1117
Stevie Frederick <leflor@gmail.com>
2011-11-07 18:05:23 -0500

    Wow. What a waste of a commit. Forgot to give a return value.

--- src/controls/IController.h
+++ src/controls/IController.h
@@ -7,7 +7,7 @@ public:
 
 virtual void dispatchEvent() = 0; //dispatches the event using controller
 //make sure you set the subID in the inherited constructor or weird stuff will happen
-virtual void getSubID() = 0;
+virtual int getSubID() = 0;
 
 private:
 virtual void Setup() = 0;
