04c380f1eb5dc52bbb5a1c54439acfcdb624f465
Stevie Frederick <leflor@gmail.com>
2011-11-07 18:04:46 -0500

    Added subID to track Controllers with in the same way as events.

--- src/controls/IController.h
+++ src/controls/IController.h
@@ -6,9 +6,12 @@ class BaseController{
 public:
 
 virtual void dispatchEvent() = 0; //dispatches the event using controller
+//make sure you set the subID in the inherited constructor or weird stuff will happen
+virtual void getSubID() = 0;
 
 private:
 virtual void Setup() = 0;
 virtual void Execute() = 0; 
 
+static int subID = 0;
 };
